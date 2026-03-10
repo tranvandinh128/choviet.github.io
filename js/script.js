954ff4030d2f2219e242a567ee7dedd1acc1a7fd
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-10 19:27:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
+function dangSanPham(){
 
+let ten = document.getElementById("ten").value;
+let gia = document.getElementById("gia").value;
+let mota = document.getElementById("mota").value;
+let sdt = document.getElementById("sdt").value;
+
+let sp = {
+ten:ten,
+gia:gia,
+mota:mota,
+sdt:sdt
+};
+
+let ds = JSON.parse(localStorage.getItem("sanpham")) || [];
+
+ds.push(sp);
+
+localStorage.setItem("sanpham",JSON.stringify(ds));
+
+alert("Đăng sản phẩm thành công");
+
+}
+
+function hienSanPham(){
+
+let ds = JSON.parse(localStorage.getItem("sanpham")) || [];
+
+let html="";
+
+ds.forEach(function(sp){
+
+html += `
+<div class="product">
+<h3>${sp.ten}</h3>
+<p>Giá: ${sp.gia}</p>
+<p>${sp.mota}</p>
+<p>Liên hệ: ${sp.sdt}</p>
+</div>
+`;
+
+});
+
+document.getElementById("danhsach").innerHTML = html;
+
+}
